535991520dbcba058fda7c3b4ae2e45d957822c4
Everest <mdrc.pops@gmail.com>
2023-12-05 16:03:26 -0600

    added a raycaster to change color of object mesh

--- main.js
+++ main.js
@@ -16,17 +16,44 @@ const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } );
 const torus = new THREE.Mesh( geometry, material );
 scene.add( torus );
 
-const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5,5,5);
+//const pointLight = new THREE.PointLight(0xffffff);
+//pointLight.position.set(5,5,5);
 
 const ambientLight = new THREE.AmbientLight(0xffffff);
 
-scene.add( pointLight, ambientLight);
+scene.add( ambientLight );
 
-const lightHelper = new THREE.PointLightHelper(pointLight);
-scene.add(lightHelper);
+//const lightHelper = new THREE.PointLightHelper(pointLight);
+//scene.add(lightHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
+
+// Interaction functionality
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+function onMouseClick(event) {
+  event.preventDefault();
+
+  // Calculate mouse position in normalized device coordinates
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  // Raycasting to check if objects are intersected
+  raycaster.setFromCamera(mouse, camera);
+
+  const intersects = raycaster.intersectObjects(scene.children);
+
+  if (intersects.length > 0) {
+    const selectedObject = intersects[0].object;
+    if (selectedObject.type === 'Mesh') {
+      const newColor = new THREE.Color(Math.random() * 0xffffff);
+      selectedObject.material.color = newColor;
+    }
+  }
+}
+
+window.addEventListener('click', onMouseClick, false);
 
 function addStar(){
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
@@ -41,16 +68,44 @@ function addStar(){
 
 Array(200).fill().forEach(addStar); 
 
-function animate() {
-	requestAnimationFrame( animate );
+// Function to add a new type of visual element - particles
+function addParticles() {
+  const particleGeometry = new THREE.BufferGeometry();
+  const particleMaterial = new THREE.PointsMaterial({
+    color: 0x888888,
+    size: 0.2,
+  });
 
-	torus.rotation.x += 0.01;
-	torus.rotation.y += 0.005;
-  //camera.rotation.x += 0.005;
+  const vertices = [];
+
+  for (let i = 0; i < 500; i++) {
+    const x = (Math.random() - 0.5) * 200;
+    const y = (Math.random() - 0.5) * 200;
+    const z = (Math.random() - 0.5) * 200;
+
+    vertices.push(x, y, z);
+  }
+
+  particleGeometry.setAttribute(
+    'position',
+    new THREE.Float32BufferAttribute(vertices, 3)
+  );
+
+  const particles = new THREE.Points(particleGeometry, particleMaterial);
+  scene.add(particles);
+}
+
+addParticles(); // Call the function to add particles to the scene
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
 
   controls.update();
 
-	renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
 
 if ( WebGL.isWebGLAvailable() ) {
